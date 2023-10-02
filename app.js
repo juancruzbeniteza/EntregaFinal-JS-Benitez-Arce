630aac384f213329a303d7fd5db2b8e43ce08cd0
juan cruz benitez arce <juancruzbeniteza@gmail.com>
2023-10-02 11:30:15 -0300

    Ultima Version

--- app.js
+++ app.js
@@ -43,14 +43,19 @@ document.addEventListener("DOMContentLoaded", function () {
         calorieRecommendation.textContent = '';
     };
 
-    // Funcion para mostrar los entrenamientos
+    let workoutHistory = [];
+
+    // Funcion para mostrar entrenamientos
     const displayWorkouts = () => {
         const workoutList = document.getElementById("workoutList");
-        workoutList.innerHTML = ""; // Limpiar la lista previa de entrenamientos
+        workoutList.innerHTML = "";
 
         workoutHistory.forEach((workout, index) => {
             const listItem = document.createElement("li");
-            listItem.textContent = `Workout ${index + 1}: ${workout.exerciseType}, Duración: ${workout.duration} minutos, Calorías Quemadas: ${workout.caloriesBurned}`;
+            listItem.innerHTML = `<strong>Entrenamiento ${index + 1}:</strong><br>
+                                  Tipo: ${workout.exerciseType}<br>
+                                  Duración: ${workout.duration} minutos<br>
+                                  Calorías Quemadas: ${workout.caloriesBurned} kcal`;
             workoutList.appendChild(listItem);
         });
     };
@@ -132,27 +137,96 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("calcularCalorias").addEventListener("click", calcularCalorias);
     document.getElementById("establecerMetas").addEventListener("click", establecerMetas);
 
+    // Event listener for "Reiniciar" button
+    const botonBorrarDatos = document.getElementById('borrarDatos');
+    botonBorrarDatos.addEventListener('click', function () {
+        Swal.fire({
+            title: '¿Seguro que quieres reiniciar?',
+            text: 'No puedes revertir este paso',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, quiero'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Borrado!',
+                    'Has reiniciado todos los datos.',
+                    'success'
+                );
+                document.getElementById("edad").value = "";
+                document.getElementById("peso").value = "";
+                document.getElementById("altura").value = "";
+                document.getElementById("sexo").value = "masculino";
+                document.getElementById("nivelActividad").value = "sedentario";
+                document.getElementById("metaPeso").value = "";
+                document.getElementById("metaActividad").value = "";
+                document.getElementById("actividadFisica").value = "";
+                document.getElementById("listaMetas").innerHTML = "";
+                localStorage.clear()
+                workoutHistory = [];
+                displayWorkouts();
+                resetCalorieInfo();
+            }
+        })
+    });
+
+    // Event listener for "Nuevo Guardado" button
+    const botonNuevoGuardado = document.getElementById("nuevoGuardado");
+    botonNuevoGuardado.addEventListener("click", function () {
+        Swal.fire({
+            title: '¿Deseas crear un nuevo guardado?',
+            text: 'Esto creará un nuevo registro en el almacenamiento local.',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, crear nuevo guardado'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const timestamp = new Date().getTime();
+                const key = `guardado_${timestamp}`;
+                localStorage.setItem(key, 'Some data for your new entry');
+
+                Swal.fire(
+                    'Guardado!',
+                    'Se ha creado un nuevo guardado.',
+                    'success'
+                );
+            }
+        });
+    });
+
+    // Event listener for "Log Workout" button
     document.getElementById("logWorkout").addEventListener("click", () => {
         const exerciseType = document.getElementById("exerciseType").value.trim();
         const duration = parseInt(document.getElementById("duration").value);
         const caloriesBurned = parseInt(document.getElementById("caloriesBurned").value);
+
         if (exerciseType === "" || isNaN(duration) || duration <= 0 || isNaN(caloriesBurned) || caloriesBurned <= 0) {
-            alert("Please enter valid workout information.");
+            alert("Por favor, introduce información válida del entrenamiento.");
             return;
         }
+
         const workout = {
             exerciseType,
             duration,
             caloriesBurned,
         };
+
         workoutHistory.push(workout);
+
+        // Clear input fields
         document.getElementById("exerciseType").value = "";
         document.getElementById("duration").value = "";
         document.getElementById("caloriesBurned").value = "";
+
+        // Display the updated workout history
         displayWorkouts();
     });
 
-    // Funcion para buscar y mostrar los datos guardados 
+    // Function to retrieve and show calorie data
     const retrieveCaloriesData = () => {
         const userDataJSON = localStorage.getItem("userData");
         if (userDataJSON) {
@@ -163,6 +237,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     };
 
+    // Function to retrieve and show fitness data
     const retrieveFitnessData = () => {
         const goalsDataJSON = localStorage.getItem("goalsData");
         if (goalsDataJSON) {
@@ -173,6 +248,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     };
 
+    // Event listeners for retrieving data
     document.getElementById("retrieveCaloriesData").addEventListener("click", retrieveCaloriesData);
     document.getElementById("retrieveFitnessData").addEventListener("click", retrieveFitnessData);
 });
+
